e429d36b988d2f656127368f7e2ca433d0b19ccb
Giacomo Pasini <giacomo@status.im>
2022-10-24 12:07:32 +0200

    only build on linux

--- examples/waku-chat/src/main.rs
+++ examples/waku-chat/src/main.rs
@@ -1,3 +1,5 @@
+#![allow(dead_code)]
+#![allow(unused)]
 // public chat service
 // messages are disseminated through waku,
 // no consensus, no blocks
@@ -29,6 +31,7 @@ struct Services {
     network: ServiceHandle<NetworkService<network::waku::Waku>>,
 }
 
+#[cfg(target_os = "linux")]
 fn main() {
     tracing_subscriber::fmt::init();
     let Args { peers, port } = Args::parse();
@@ -40,4 +43,9 @@ fn main() {
         None,
     );
     app.wait_finished();
+}
+
+#[cfg(not(target_os = "linux"))]
+fn main() {
+    println!("waku is only supported on linux");
 }
