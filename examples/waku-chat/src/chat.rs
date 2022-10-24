cc20ecc91855616d647b9ae45759ef21b19e37fc
Giacomo Pasini <Zeegomo@users.noreply.github.com>
2022-10-24 13:21:39 +0200

    Add waku-chat example (#1)
    * add waku-chat example
    
    * clippy happy
    
    * update dependencies
    
    * remove unneeded tokio features
    
    * use separate thread for stdin
    
    * only build on linux
    
    Co-authored-by: Giacomo Pasini <giacomo@status.im>

---
+++ examples/waku-chat/src/chat.rs
@@ -0,0 +1,90 @@
+use crate::network::*;
+use async_trait::async_trait;
+use overwatch::services::handle::ServiceStateHandle;
+use overwatch::services::relay::{NoMessage, Relay};
+use overwatch::services::state::{NoOperator, NoState};
+use overwatch::services::{ServiceCore, ServiceData, ServiceId};
+use serde::{Deserialize, Serialize};
+use tokio::sync::mpsc::channel;
+
+/// Chat service handler
+/// displays received messages, send new ones
+pub struct ChatService {
+    service_state: ServiceStateHandle<Self>,
+}
+
+#[derive(Deserialize, Serialize)]
+struct Message {
+    user: usize,
+    msg: Box<[u8]>,
+}
+
+impl ServiceData for ChatService {
+    const SERVICE_ID: ServiceId = "Chat";
+    type Settings = usize;
+    type State = NoState<Self::Settings>;
+    type StateOperator = NoOperator<Self::State>;
+    type Message = NoMessage;
+}
+
+#[async_trait]
+impl ServiceCore for ChatService {
+    fn init(service_state: ServiceStateHandle<Self>) -> Self {
+        Self { service_state }
+    }
+
+    async fn run(self) {
+        let Self {
+            mut service_state, ..
+        } = self;
+        // TODO: waku should not end up in the public interface of the network service, at least not as a type
+        let mut network_relay: Relay<NetworkService<waku::Waku>> =
+            service_state.overwatch_handle.relay();
+        let user = service_state.settings_reader.get_updated_settings();
+        network_relay.connect().await.unwrap();
+        let (sender, mut receiver) = channel(1);
+        // TODO: typestate so I can't call send if it's not connected
+        network_relay
+            .send(NetworkMsg::Subscribe {
+                kind: EventKind::Message,
+                sender,
+            })
+            .await
+            .unwrap();
+
+        // send new messages
+        // for interactive stdin I/O it's recommended to
+        // use an external thread, see https://docs.rs/tokio/latest/tokio/io/struct.Stdin.html
+        std::thread::spawn(move || loop {
+            let mut input = String::new();
+            std::io::stdin()
+                .read_line(&mut input)
+                .expect("error reading message");
+            input.truncate(input.trim().len());
+            network_relay
+                .blocking_send(NetworkMsg::Broadcast(
+                    bincode::serialize(&Message {
+                        user,
+                        msg: input.as_bytes().to_vec().into_boxed_slice(),
+                    })
+                    .unwrap()
+                    .into_boxed_slice(),
+                ))
+                .unwrap();
+            tracing::debug!("[sending]: {}...", input);
+        });
+
+        // print received messages
+        while let Some(NetworkEvent::RawMessage(message)) = receiver.recv().await {
+            if let Ok(msg) = bincode::deserialize::<Message>(&message) {
+                if msg.user != user {
+                    println!(
+                        "[received][{}]: {}",
+                        msg.user,
+                        String::from_utf8_lossy(&msg.msg)
+                    );
+                }
+            }
+        }
+    }
+}
---
+++ examples/waku-chat/src/main.rs
@@ -0,0 +1,51 @@
+#![allow(dead_code)]
+#![allow(unused)]
+// public chat service
+// messages are disseminated through waku,
+// no consensus, no blocks
+mod network;
+// TODO: different chat rooms with different contentTopicId
+mod chat;
+
+use chat::*;
+use clap::Parser;
+use network::*;
+use overwatch::{overwatch::*, services::handle::ServiceHandle};
+use overwatch_derive::*;
+
+/// Simple program to greet a person
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+struct Args {
+    /// Multiaddrs of other nodes participating in the protocol
+    #[arg(short, long)]
+    peers: Vec<String>,
+
+    /// Listening port
+    port: u16,
+}
+
+#[derive(Services)]
+struct Services {
+    chat: ServiceHandle<ChatService>,
+    network: ServiceHandle<NetworkService<network::waku::Waku>>,
+}
+
+#[cfg(target_os = "linux")]
+fn main() {
+    tracing_subscriber::fmt::init();
+    let Args { peers, port } = Args::parse();
+    let app = OverwatchRunner::<Services>::run(
+        ServicesServiceSettings {
+            chat: rand::random(),
+            network: NetworkConfig { peers, port },
+        },
+        None,
+    );
+    app.wait_finished();
+}
+
+#[cfg(not(target_os = "linux"))]
+fn main() {
+    println!("waku is only supported on linux");
+}
