bef6f8c5c26bedfc4d717f47c16f1f3e2d52ff30
Alejandro Cabeza Romero <alex93cabeza@gmail.com>
2024-12-30 13:13:57 +0100

    Update PingPong example state recovery mechanism.

--- examples/ping_pong/src/states.rs
+++ examples/ping_pong/src/states.rs
@@ -1,5 +1,3 @@
-// STD
-use std::io;
 // Crates
 use overwatch_rs::services::state::ServiceState;
 use serde::{Deserialize, Serialize};
@@ -14,22 +12,11 @@ pub struct PingState {
     pub pong_count: u32,
 }
 
-impl PingState {
-    fn load_saved_state(save_path: &str) -> io::Result<Self> {
-        let json_state = std::fs::read(save_path)?;
-        let state = serde_json::from_slice(json_state.as_slice())
-            .map_err(|error| io::Error::new(io::ErrorKind::InvalidData, error))?;
-        Ok(state)
-    }
-}
-
 impl ServiceState for PingState {
     type Settings = PingSettings;
     type Error = PingStateError;
 
-    fn from_settings(settings: &Self::Settings) -> Result<Self, Self::Error> {
-        let state = Self::load_saved_state(settings.state_save_path.as_str())
-            .unwrap_or_else(|_error| Self::default());
-        Ok(state)
+    fn from_settings(_settings: &Self::Settings) -> Result<Self, Self::Error> {
+        Ok(Self::default())
     }
 }
