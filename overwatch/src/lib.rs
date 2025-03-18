57d280f8869e43dd168ac98c5e6f2a286ffd1a81
Antonio Antonino <antonio@kilt.io>
2025-03-18 08:49:46 +0100

    Update overwatch

--- overwatch/src/lib.rs
+++ overwatch/src/lib.rs
@@ -7,15 +7,17 @@ pub mod services;
 pub mod utils;
 
 pub type DynError = Box<dyn std::error::Error + Send + Sync + 'static>;
-pub type OpaqueServiceHandle<S> = services::handle::ServiceHandle<
+pub type OpaqueServiceHandle<S, AggregateServiceId> = services::handle::ServiceHandle<
     <S as ServiceData>::Message,
     <S as ServiceData>::Settings,
     <S as ServiceData>::State,
+    AggregateServiceId,
 >;
-pub type OpaqueServiceStateHandle<S> = services::handle::ServiceStateHandle<
+pub type OpaqueServiceStateHandle<S, AggregateServiceId> = services::handle::ServiceStateHandle<
     <S as ServiceData>::Message,
     <S as ServiceData>::Settings,
     <S as ServiceData>::State,
+    AggregateServiceId,
 >;
 
 #[cfg(feature = "derive")]
