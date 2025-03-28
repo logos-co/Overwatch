0cf988d1ec978c472ef9835bca156d931ebab102
Antonio <antonio@status.im>
2025-03-28 10:43:46 +0100

    chore: pin actions (#70)

--- shell.nix
+++ shell.nix
@@ -1,12 +1,12 @@
 { pkgs ? import <nixpkgs> {
     builtins = [(import (fetchTarball {
-      url    = "https://github.com/NixOS/nixpkgs/archive/3389f23412877913b9d22a58dfb241684653d7e9.tar.gz";
-      sha256 = "sha256:0wgm7sk9fca38a50hrsqwz6q79z35gqgb9nw80xz7pfdr4jy9pf8";
+      url    = "https://github.com/NixOS/nixpkgs/archive/e06c5e01088672bc460b2bc6b61d88e95190a492.tar.gz";
+      sha256 = "sha256:e7d37547638aeb6b70a9dbf6dcc5970529edef39b46760a1c9689ac7f066ed58";
     }))];
     overlays = [
       (import (fetchGit {
         url = "https://github.com/oxalica/rust-overlay.git";
-	rev = "fe185fac76e009b4bd543704a8c61077cf70155b";
+        rev = "a0e3395c63cdbc9c1ec17915f8328c077c79c4a1";
       }))
     ];
    }
@@ -17,7 +17,9 @@ pkgs.mkShell {
 
   buildInputs = with pkgs; [
     pkg-config
-    rust-bin.stable."1.65.0".default
+    # Updating the version here requires also updating the `rev` version in the `overlays` section above
+    # with a commit that contains the new version in its manifest
+    rust-bin.stable."1.85.1".default
     go_1_19
   ];
 }
