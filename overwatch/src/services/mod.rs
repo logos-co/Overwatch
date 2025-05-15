83a5efd3e84c335c770da2d1e084b82a2aa54a71
Alejandro Cabeza Romero <alex93cabeza@gmail.com>
2025-05-15 19:32:53 +0200

    Services mod overview doc

--- overwatch/src/services/mod.rs
+++ overwatch/src/services/mod.rs
@@ -1,3 +1,26 @@
+/*!
+# Services
+This is a brief overview of the main entities of the Services module:
+- [`ServiceRunner`](runner::ServiceRunner): Oneshot runner of a `Service`.
+  When booted, it starts listening for [`LifecycleMessage`](life_cycle::LifecycleMessage)s for that
+  `Service` and acts upon them.
+  Currently: `Start` and `Stop` the `Service`.
+- [`ServiceHandle`](handle::ServiceHandle): Contains the components an external source might need
+  to communicate with a `Service`.
+  It includes everything from mechanisms to send `Message`s to `Service`s to reading and updating
+  their `Settings`.
+- [`ServiceResources`](resources::ServiceResources): Contains the components a `Service` and
+  [`ServiceRunner`](runner::ServiceRunner) might need to interact with itself and
+  [`Overwatch`](super::overwatch::Overwatch).
+  It includes everything from mechanisms to send
+  [`OverwatchCommand`](super::overwatch::commands::OverwatchCommand)s to updating their own `State`.
+  The ownership of [`ServiceResources`](resources::ServiceResources) belongs to
+  [`ServiceRunner`](runner::ServiceRunner).
+  - [`ServiceResourcesHandle`]: A clone from [`ServiceResources`](resources::ServiceResources)
+    that includes the [`InboundRelay`](relay::InboundRelay) for the `Service`.
+    Whenever a `Service` is started, a new clone is made.
+ */
+
 pub mod handle;
 pub mod life_cycle;
 pub mod relay;
@@ -29,9 +52,9 @@ pub trait ServiceData {
 /// Trait implemented for services that are included in a specific Overwatch
 /// handle by the aggregated runtime service ID.
 // This trait is implemented by the runtime macro and must be required by
-// services to be able to communicate with each other. This trait basically
-// guarantees that services implementing this for the same runtime service ID,
-// are indeed part of the same runtime.
+// services to be able to communicate with each other.
+// It guarantees that services implementing this for the same RuntimeServiceId
+// are part of the same runtime.
 pub trait AsServiceId<T> {
     const SERVICE_ID: Self;
 }
