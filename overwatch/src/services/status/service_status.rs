b70b3f8451a4a3dd4496044cdd82e3e07d3d390a
Álex <alex93cabeza@gmail.com>
2025-06-09 09:53:48 +0000

    chore(service-status): Implement Display for ServiceStatus. (#102)

--- overwatch/src/services/status/service_status.rs
+++ overwatch/src/services/status/service_status.rs
@@ -1,3 +1,5 @@
+use std::fmt::{Display, Formatter};
+
 #[derive(Copy, Clone, Eq, PartialEq, Debug)]
 pub enum ServiceStatus {
     /// The `Service` is in the process of being started.
@@ -12,4 +14,16 @@ pub enum ServiceStatus {
     /// It can be restarted by sending the appropriate
     /// [`LifecycleMessage`](crate::services::lifecycle::LifecycleMessage).
     Stopped,
+}
+
+impl Display for ServiceStatus {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        use ServiceStatus::{Ready, Starting, Stopped};
+        let service_status = match self {
+            Starting => "ServiceStatus::Starting",
+            Ready => "ServiceStatus::Ready",
+            Stopped => "ServiceStatus::Stopped",
+        };
+        write!(f, "{service_status}")
+    }
 }
