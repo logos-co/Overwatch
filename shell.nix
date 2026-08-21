b8ecd21a6e6756880ad880afd07874989b13c9f4
Antonio Antonino <antonio@status.im>
2026-08-21 08:18:16 +0200

    Update

--- shell.nix
+++ shell.nix
@@ -6,7 +6,7 @@
     overlays = [
       (import (fetchGit {
         url = "https://github.com/oxalica/rust-overlay.git";
-        rev = "b99d48435bc3e34309d2c7ae6f7d45e77a156c38";
+        rev = "89abdfd661ea493cde2f73d7b1332cb34150aa43";
       }))
     ];
    }
@@ -19,7 +19,7 @@ pkgs.mkShell {
     pkg-config
     # Updating the version here requires also updating the `rev` version in the `overlays` section above
     # with a commit that contains the new version in its manifest
-    rust-bin.stable."1.97.1".default
+    rust-bin.stable."1.98.0".default
     go_1_19
   ];
 }
